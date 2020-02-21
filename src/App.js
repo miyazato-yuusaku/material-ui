8c944854790b39814ccf04d92e806a74e5a0c369
yuusaku <yuusaku@example.com>
2020-02-21 15:03:44 +0900

    かんせい

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends React.Component {
     super(props)
     this.state = {
       items: {},
-      item: ''
+      item: '',
+      rist: ''
     }
     this.apikey = 'd44820a99e565793f96bb49913a3135f'
   }
@@ -26,18 +27,51 @@ class App extends React.Component {
     const index = e.target.dataset.optionIndex
     const data = this.state.items[index].areacode_s
     this.setState({ item: data })
+    this.searchData()
     console.log(data)
   }
 
-  componentDidMount () {
-    getJSON(
-      'https://api.gnavi.co.jp/master/GAreaSmallSearchAPI/v3/?keyid=d44820a99e565793f96bb49913a3135f&lang=ja'
-    )
-      .then(json => json.garea_small)
-      .then(json => this.setState({ items: json }))
+  async setData () {
+    const getJSON = uri =>
+      window
+        .fetch(uri)
+        .then(res => res.json())
+        .then(json => json.garea_small)
+
+    const uri = 'https://api.gnavi.co.jp/master/GAreaSmallSearchAPI/v3/?'
+    const params = `keyid=${this.apikey}&lang=ja`
+    const data = await getJSON(uri + params)
+    this.setState({ items: data })
   }
 
+  async searchData () {
+    const getJSON = uri =>
+      window
+        .fetch(uri)
+        .then(res => res.json())
+        .then(json => json.rest)
+        .then(json => json[0].name)
+
+    const uri = 'https://api.gnavi.co.jp/RestSearchAPI/v3/?'
+    const params = `keyid=${this.apikey}&areacode_s=${this.state.item}`
+    const data = await getJSON(uri + params)
+    this.setState({ rist: data })
+  }
+
+  componentDidMount () {
+    this.setData()
+  }
+
+  //  componentDidMount () {
+  //    getJSON(
+  //      'https://api.gnavi.co.jp/master/GAreaSmallSearchAPI/v3/?keyid=d44820a99e565793f96bb49913a3135f&lang=ja'
+  //    )
+  //      .then(json => json.garea_small)
+  //      .then(json => this.setState({ items: json }))
+  //  }
+
   render () {
+    console.log(this.state.rist)
     return (
       <Card>
         <CardHeader title='ぐるなび' />
@@ -48,7 +82,7 @@ class App extends React.Component {
           />
         </CardActions>
         <CardContent>
-          <ListView item={this.state.item} />
+          <ListView rist={this.state.rist} />
         </CardContent>
       </Card>
     )
@@ -56,7 +90,7 @@ class App extends React.Component {
 }
 
 const ListView = props => {
-  const data = props.item
+  const data = props.rist
   return (
     <List>
       <ListItem>
@@ -66,10 +100,15 @@ const ListView = props => {
   )
 }
 
-const getJSON = async uri => {
-  const result = await window.fetch(uri).then(res => res.json())
-  return result
-}
+// const getJSON = async uri => {
+//  const result = await window.fetch(uri).then(res => res.json())
+//  return result
+// }
+
+// const getJson = (uri, params, options) => {
+//  const result = window.fetch(uri + params, options).then(res => res.json())
+//  return result
+// }
 
 const SelectorView = props => (
   <Autocomplete
